e97e1daaddee89285715c1a5b541b54b3d55cc5e
Rishabh Rao <71926015+rishabhrao12@users.noreply.github.com>
2021-10-01 18:13:21 +0530

    Delete model-testing.py

--- model-testing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import warnings
-from statsmodels.tsa.arima.model import ARIMAResults
-import matplotlib.pyplot as plt
-
-model_fit = ARIMAResults.load('ARIMA_Model.pkl')
-model_fit.plot_predict(dynamic=False)
-plt.show()

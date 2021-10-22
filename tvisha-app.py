0c0770b68e3802c2043510013f6831338f4e8122
Rishabh Rao <rishabhrao12@gmail.com>
2021-10-22 11:02:12 +0530

    updated tvisha

--- tvisha-app.py
+++ tvisha-app.py
@@ -1,11 +1,7 @@
-# Run this app with `tvisha python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-# Tvisha change the html and css elements to change the design
-
-
 import dash
-from dash import dcc
+from dash import dcc, callback_context
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
@@ -22,12 +18,11 @@ colors = {
     'text': 'black'
 }
 
-df = pd.read_csv('NFLX.csv')
-
-filename = "ridge_regression.sav"
+filename = "linear_regression_aapl.pickle"
 
 model = pickle.load(open(filename, 'rb'))
 
+df = pd.read_csv('NFLX.csv')
 candlestick = go.Figure(data=[go.Candlestick(x=df['Date'],
                                              open=df['Open'],
                                              high=df['High'],
@@ -54,7 +49,7 @@ app.layout = html.Div([
             ),
             html.Hr(className="my-2"),
             html.P(dbc.Button("Predict Now!", color="primary"), className="lead"),
-            html.H4("Currently showing Netflix(NFLX)", className="display-3", style={"font-size": "20px"}),
+            html.H4("Stock Analytic Dashboard", className="display-3", style={"font-size": "20px"}),
         ],
     ),
     html.Hr(),
@@ -65,36 +60,47 @@ app.layout = html.Div([
             {'label': 'AMAZON', 'value': 'AMZN.csv'},
             {'label': 'APPLE', 'value': 'AAPL.csv'},
             {'label': 'FACEBOOK', 'value': 'FB.csv'},
-            {'label': 'GOOGLE', 'value': 'GOOG.csv'}
+            {'label': 'GOOGLE', 'value': 'GOOG-2.csv'}
         ],
-        value='NFLX,csv'
+        value='NFLX.csv'
     ),
-    html.Div(id='dd-output-container'),
-
-    dcc.Graph(
-        id='CANDLESTICK',
-        figure=candlestick
+    dcc.Dropdown(
+        id='date',
+        options=[
+            {'label': '6mo', 'value': '25'},
+            {'label': '1y', 'value': '50'},
+            {'label': '2y', 'value': '300'},
+            {'label': '5y', 'value': 'default'}
+        ],
+        value='NFLX.csv'
+    ),
+    html.Div(
+        id='dd-output-container',
+        children=html.Div(dcc.Graph(
+            id='CANDLESTICK',
+            figure=candlestick
+        ))
     ),
 
     # html.Hr(),
 
     html.Div(children='Predicting Close Value ', style={
         'textAlign': 'center',
-        'color': 'white',
+        'color': 'black',
         'font-size': "24px",
         'font-family': 'Trebuchet MS',
-        'background-color': '#1E90FF',
+        'background-color': '#F5F5F5',
         'margin': 'auto',
         'text-indent': '0px',
         'height': '70px',
 
     }),
-    html.Div(style={'background-color': '#1E90FF'}, children=[
+    html.Div(style={'background-color': '#F5F5F5'}, children=[
         html.Span(
             children='Input Open Price ($): ',
             style={
                 'margin-left': '150px',
-                'color': 'white',
+                'color': 'black',
                 "font-size": "20px",
                 'font-family': 'Trebuchet MS',
 
@@ -115,7 +121,7 @@ app.layout = html.Div([
                 "margin-left": '180px',
                 "font-size": "20px",
                 'font-family': "Trebuchet MS",
-                'color': 'white',
+                'color': 'black',
             },
         ),
 
@@ -124,6 +130,7 @@ app.layout = html.Div([
         # html.Br(),
         # html.Br(),
         html.Br(),
+        html.Br(),
 
         html.Span(
             children="Model Accuracy: 99.16%",
@@ -131,14 +138,13 @@ app.layout = html.Div([
             style={
                 "font-size": "20px",
                 'font-family': 'Trebuchet MS',
-                'color': 'white',
+                'color': 'black',
                 'margin-left': '520px',
-
             },
         ),
         html.Div(
             [
-                dbc.Progress(value=99, color="warning", className="mb-3", style={"width": "2000px"}),
+                dbc.Progress(value=90, color="black", className="mb-3", style={"width": "2000px"}),
 
             ]
         )
@@ -146,11 +152,13 @@ app.layout = html.Div([
 
     html.Br(),
     html.Br(),
-
-    dcc.Graph(
-        id='CLOSE-OPEN',
-        figure=line_graph,
-    ),
+    html.Div(
+        id='button-output-container',
+        children=html.Div(dcc.Graph(
+            id='CLOSE-OPEN',
+            figure=line_graph,
+        ),
+        )),
     html.Hr(style={"color": "blue", "size": "10px"}),
     dbc.CardDeck(
         [
@@ -164,7 +172,7 @@ app.layout = html.Div([
                             className="card-text",
                         ),
                         dbc.Button(
-                            "Click here", color="primary", className="mt-auto"
+                            "View Graph", color="primary", className="mt-auto", id="AM-button", n_clicks=0, name="AMZN.csv"
                         ),
                     ]
                 )
@@ -178,7 +186,7 @@ app.layout = html.Div([
                             className="card-text",
                         ),
                         dbc.Button(
-                            "Click here", color="primary", className="mt-auto"
+                            "View Graph", color="primary", className="mt-auto", id="AP-button", n_clicks=0, name="AAPL.csv"
                         ),
                     ]
                 )
@@ -193,7 +201,7 @@ app.layout = html.Div([
                             className="card-text",
                         ),
                         dbc.Button(
-                            "Click here", color="primary", className="mt-auto"
+                            "View Graph", color="primary", className="mt-auto", id="FB-button", n_clicks=0, name="FB.csv"
                         ),
                     ]
                 )
@@ -209,7 +217,23 @@ app.layout = html.Div([
                             className="card-text",
                         ),
                         dbc.Button(
-                            "Click here", color="primary", className="mt-auto"
+                            "View Graph", color="primary", className="mt-auto", id="GOOG-button", n_clicks=0, name="GOOG-2.csv"
+                        ),
+                    ]
+                )
+            ),
+            dbc.Card(
+                dbc.CardBody(
+                    [
+
+                        html.H5("Netflix", className="card-title"),
+                        html.P(
+                            "To check stocks from over 5 years and predict to make better investments",
+
+                            className="card-text",
+                        ),
+                        dbc.Button(
+                            "View Graph", color="primary", className="mt-auto", id="NF-button", n_clicks=0, name="NFLX.csv"
                         ),
                     ]
                 )
@@ -220,6 +244,7 @@ app.layout = html.Div([
 ])
 
 
+# Open and Close Predictor
 @app.callback(
     Output(component_id="predicted_close", component_property="children"),
     Input(component_id="input_value", component_property="value")
@@ -232,13 +257,61 @@ def update_output_div(input_value):
     return 'Predicted Close Price ($): {}'.format(close_price)
 
 
+# Scatter plot
+@app.callback(
+    Output('button-output-container', 'children'),
+    Input('AM-button', 'n_clicks'),
+    Input('AP-button', 'n_clicks'),
+    Input('FB-button', 'n_clicks'),
+    Input('GOOG-button', 'n_clicks'),
+    Input('NF-button', 'n_clicks')
+)
+def update_output(btn1,btn2,btn3,btn4,btn5):
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+    if 'AM-button' in changed_id:
+        msg = 'AMZN.csv'
+    elif 'AP-button' in changed_id:
+        msg = 'AAPL.csv'
+    elif 'FB-button' in changed_id:
+        msg = 'FB.csv'
+    elif 'GOOG-button' in changed_id:
+        msg = 'GOOG-2.csv'
+    elif 'NF-button' in changed_id:
+        msg = 'NFLX.csv'
+    else:
+        msg = 'NFLX.csv'
+    df = pd.read_csv(msg)
+    line_graph = px.scatter(x=df['Open'], y=df['Close'], labels={'x': 'Open', 'y': 'Close'})
+    line_graph.update_layout(title='OPEN v CLOSE', title_x=0.5)
+    return html.Div(dcc.Graph(
+        id='CLOSE-OPEN',
+        figure=line_graph
+    ))
+
+
+# Dropdown
 @app.callback(
     Output('dd-output-container', 'children'),
     Input('demo-dropdown', 'value')
 )
 def update_output(value):
     # df = pd.read_csv(value)
-    return 'You have selected "{}"'.format(value)
+    df = pd.read_csv(value)
+    candlestick = go.Figure(data=[go.Candlestick(x=df['Date'],
+                                                 open=df['Open'],
+                                                 high=df['High'],
+                                                 low=df['Low'],
+                                                 close=df['Close'])])
+
+    candlestick.update_layout(title='CANDLESTICK GRAPH', title_x=0.5)
+
+    return html.Div(dcc.Graph(
+        id='CANDLESTICK',
+        figure=candlestick
+    ))
+
+    # line_graph = px.scatter(x=df['Open'], y=df['Close'], labels={'x': 'Open', 'y': 'Close'})
+    # line_graph.update_layout(title='OPEN v CLOSE', title_x=0.5)
 
 
 if __name__ == '__main__':
